a7b724ea378dfa6ceba856df95deae71b0d07006
Mohammed Al-Oraibi <mohamed.b.oraibi@gmail.com>
2019-09-14 09:05:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+document.getElementById("main").remove();
+
+let element = document.createElement('h1');
+document.body.appendChild(element);
+document.getElementsByTagName("h1")[0].setAttribute("id", "victory");
+let newHeader = document.getElementById('victory');
+let NAME="Mohammed"
+document.getElementById("victory").innerHTML=`${NAME} is the champion`;
