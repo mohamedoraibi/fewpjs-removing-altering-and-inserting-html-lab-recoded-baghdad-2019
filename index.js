faf4a55dae55274619327550bd3d7eeead74fd88
Mohammed Al-Oraibi <mohamed.b.oraibi@gmail.com>
2019-09-14 09:01:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ let element = document.createElement('h1');
 document.body.appendChild(element);
 document.getElementsByTagName("h1")[0].setAttribute("id", "victory");
 let newHeader = document.getElementById('victory');
-document.getElementById("victory").innerHTML('is the champion');
+let NAME="mohamed"
+document.getElementById("victory").innerHTML(`${NAME} is the champion`);
